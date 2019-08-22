d6a246dde91a719bde10849f645012ed04453730
DannyW9 <dannywelsh400@hotmail.com>
2019-08-22 16:56:38 +0100

    Adds ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,37 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg'),
-    new Recipe('Second Test', 'A second test recipe', 'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+    new Recipe(
+      'Garlic Shrimp',
+      'A flavorful shrimp dish that can be ready in a few minutes.',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg',
+    [
+      new Ingredient('Prawns', 15),
+      new Ingredient('Garlic', 4)
+    ]),
+    new Recipe(
+      'Buffalo Cauliflower Tacos',
+      'Hot & Spicy buffalo cauliflower tacos with avocado crema',
+      'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Crema-Recipe.jpg',
+    [
+      new Ingredient('Buffalo Cauliflower', 3),
+      new Ingredient('Avocado', 2)
+    ]),
+    new Recipe(
+      'Asian Chicken Wings',
+      'Sticky AND Crispy Asian chicken wings. The perfect party food!',
+      'https://www.kitchensanctuary.com/wp-content/uploads/2016/06/Crispy-Chicken-Wings-with-Sticky-Asian-Glaze-square-FS-300x300.jpg',
+      [
+        new Ingredient('Chicken Wings', 20),
+        new Ingredient('Onion', 4)
+      ]
+    )
   ];
 
   getRecipes() {
