38e6e6a3c91307badd76fe03244e20ec1d69bb51
DannyW9 <dannywelsh400@hotmail.com>
2019-08-22 16:06:26 +0100

    Moves recipe array from list component to recipe service. Makes recipe private

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,15 @@
-
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg'),
+    new Recipe('Second Test', 'A second test recipe', 'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
 }
+
+// Manage recipes
